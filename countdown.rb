90fbb39c2cb73e98e5d60aaa381dd2bcf4e909a1
Paloma Mirta Mendez <mpaloma2014@gmail.com>
2020-07-28 22:29:43 +0000

    Automatically backed up by Learn

--- countdown.rb
+++ countdown.rb
@@ -1,5 +1,5 @@
 def countdown(num)
-  while num > 0 do
+  while num > 0
   puts "#{num} SECONDS(S)!"
   num -= 1 
 end
@@ -7,7 +7,7 @@ end
 end
 
 def countdown_with_sleep(num)
-  while num > 0 do
+  while num > 0
   puts "#{num} SECOND(S)!"
   sleep(1)
   num -= 1 
