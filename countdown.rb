cb025784a8815807e9da76915988a6dcd5eaf25e
Paloma Mirta Mendez <mpaloma2014@gmail.com>
2020-07-28 22:02:39 +0000

    Automatically backed up by Learn

--- countdown.rb
+++ countdown.rb
@@ -1,4 +1,15 @@
-#write your code here
+def countdown(num)
+  while num > 0 do
+  puts "#{num} SECONDS(S)!"
+  num -= 1 
+end
+    "HAPPY NEW YEAR!"
+end
 
-def countdown
+def countdown_with_sleep(num)
+  while num > 0 do
+  puts "#{num} SECOND(S)!"
+  sleep(1)
+  num -= 1 
+  end
 end
